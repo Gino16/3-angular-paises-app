c38110a21d6c13981da336d927f8d997d12d55f4
Gino AScencio <gino.francisco@hotmail.com>
2022-07-10 15:16:25 -0500

    add: Por Capital works

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -17,4 +17,8 @@ export class PaisService {
     return this.http.get<Country[]>(`${this.apiUrl}/name/${termino}`);
   }
 
+  buscarCapital(termino: string): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiUrl}/capital/${termino}`)
+  }
+
 }
